cc494fe808122acf4d0f936efccd932219035237
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2024-04-12 22:03:48 +0500

    Add files via upload

---
+++ stars.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main(){
+    int n,i;
+        std::cin>>n;
+        
+    for( i=1;i<=n;i++){
+        for (int  j=1;j<=i;j++)
+         {   std::cout<<'*';
+        
+            /* code */
+        }
+        int space=2*n-2*i;
+for (int i = 0; i < space; i++)
+{std::cout<<" ";
+    /* code */
+}
+ for (int  j=1;j<=i;j++)
+         {   std::cout<<'*';
+        
+            /* code */
+        }
+
+        std::cout<<std::endl;
+    }
+    
+
+        for( i=1;i<=n;i++){
+        for (int  j=1;j<=i;j++)
+         {   std::cout<<'*';
+        
+            /* code */
+        }
+        int space=2*n-2*i;
+for (int i = 0; i < space; i++)
+{std::cout<<" ";
+    /* code */
+}
+ for (int  j=1;j<=i;j++)
+         {   std::cout<<'*';
+        
+            /* code */
+        }
+
+        std::cout<<std::endl;
+    }
+}
