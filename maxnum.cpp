b72de0960ae36fba5e0385df0d6a423becb88cc6
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2024-04-24 21:29:13 +0500

    Add files via upload

---
+++ maxnum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <climits>
+int main(){
+int arr[]={12 ,34 ,5 ,54};
+int n=5;
+for (int i = 0; i < n-1; i++)
+{std::cout<<arr[i]<<std::endl;
+}
+
+    
+    
+   
+   
+   
+
+    
+}
