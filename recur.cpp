42133a770824bb6036b7bf19dee20bee34dd4715
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2024-05-01 00:22:01 +0500

    Add files via upload
    resursion

---
+++ recur.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+bool function(int arr[],int size){
+    if(size==1){
+        return true;
+    }
+bool check{
+    std::sort(arr[]-1,size-1);
+    return true;
+}
+    return (arr[0]<arr[1]&& check);
+}
