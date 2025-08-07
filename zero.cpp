22d3cdd15a5c328ceac89a8d18aa1447bf060e21
Syed Ahmed Shaheer Ali <137728156+sa-shaheer-ali@users.noreply.github.com>
2025-08-07 23:45:43 +0500

    Add files via upload
    moving all the zero in a vector to the end!

---
+++ zero.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+void moveZeroes(std::vector<int>& nums){
+    int index=0;
+    for(size_t i=0;i<nums.size();++i){
+        if(nums[i]!=0){
+            nums[index]=nums[i];
+            ++index;
+
+        }
+    }
+        while(index<nums.size()){
+            nums[index++]=0;
+
+        }
+
+}
+
+int main (){
+    std::vector<int>nums={0,4,0,1,0,4,6};
+    moveZeroes(nums);
+    for(size_t num:nums){
+    std::cout<<num;
+}
+return 0;
+}
